7e4947dab7bc7ea7ce64169f6650dc02182a79c5
sefaisen <sefa@volitek.com.tr>
2022-03-05 23:22:28 +0300

    Discount service

---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,64 @@
+﻿using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+        private readonly IDiscountService _discountservice;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountsController(IDiscountService discountservice, ISharedIdentityService sharedIdentityService)
+        {
+            _discountservice = discountservice;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResultInstance(await _discountservice.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResultInstance(await _discountservice.GetById(id));
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var userId = _sharedIdentityService.GetUserId;
+            return CreateActionResultInstance(await _discountservice.GetByCodeAndUserId(code, userId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountservice.Add(discount));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountservice.Update(discount));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResultInstance(await _discountservice.Delete(id));
+        }
+    }
+}
---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Models.Discount>>> GetAll();
+        Task<Response<Models.Discount>> GetById(int id);
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userid);
+        Task<Response<NoContent>> Add(Models.Discount model);
+        Task<Response<NoContent>> Update(Models.Discount model);
+        Task<Response<NoContent>> Delete(int id);
+    }
+}
