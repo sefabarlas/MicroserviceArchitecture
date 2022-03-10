47bfeed5fc8bbe247a93e24d5f9f7cb50ab0b9f2
sefaisen <sefa@volitek.com.tr>
2022-03-10 23:01:43 +0300

    configuration.developmets.json routes and Gateway is under protection

--- Gateways/FreeCourse.Gateway/Startup.cs
+++ Gateways/FreeCourse.Gateway/Startup.cs
@@ -1,12 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +18,28 @@ namespace FreeCourse.Gateway
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //kullanýcý olma zorunluluðu ile ilgili policy
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+            services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options => {
+                options.Authority = Configuration["IdentityServerUrl"];
+                options.Audience = "resource_gateway";
+                options.RequireHttpsMetadata = false;
+            });
+
             services.AddOcelot();
         }
 
