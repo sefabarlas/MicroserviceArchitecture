8b40dc2b739a827c4ba0128985ca88c0ff6ef165
sefaisen <sefa@volitek.com.tr>
2022-03-12 17:44:41 +0300

    TokenDelegateHandler/ResourceOwnerPasswordTokenHandler

---
+++ Frontends/FreeCourse.Web/Exceptions/UnAuthorizeException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Exceptions
+{
+    public class UnAuthorizeException : Exception
+    {
+        public UnAuthorizeException()
+        {
+        }
+
+        public UnAuthorizeException(string message) : base(message)
+        {
+        }
+
+        public UnAuthorizeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnAuthorizeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
