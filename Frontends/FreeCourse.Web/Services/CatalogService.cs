e425a49499facdebb371b3cc2e3910bf365222e8
sefaisen <sefa@volitek.com.tr>
2022-03-13 13:32:39 +0300

    CatalogService

--- Frontends/FreeCourse.Web/Services/CatalogService.cs
+++ Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -1,12 +1,56 @@
-﻿using FreeCourse.Web.Services.Interfaces;
+﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Models.Catalog;
+using FreeCourse.Web.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
 {
     public class CatalogService : ICatalogService
     {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CategoryViewModel>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<CourseViewModel>> GetAllCourseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CourseViewModel> GetCourseByIdAsync(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteCourseAsync()
+        {
+            throw new NotImplementedException();
+        }   
     }
 }
--- Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -8,6 +8,7 @@ namespace FreeCourse.Web.Services.Interfaces
 {
     public interface ICatalogService
     {
+        Task<List<CategoryViewModel>> GetAllCategoryAsync();
         Task<List<CourseViewModel>> GetAllCourseAsync();
         Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
         Task<CourseViewModel> GetCourseByIdAsync(string courseId);
