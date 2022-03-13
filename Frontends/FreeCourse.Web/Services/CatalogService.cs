d2701bfe79a5a14e94922a1d89fa844bccadbd06
sefaisen <sefa@volitek.com.tr>
2022-03-13 13:26:18 +0300

    ICatalogService

---
+++ Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class CatalogService : ICatalogService
+    {
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Web.Models.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseViewModel>> GetAllCourseAsync();
+        Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
+        Task<CourseViewModel> GetCourseByIdAsync(string courseId);
+        Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
+        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
+        Task<bool> DeleteCourseAsync();
+    }
+}
